3b444955939010effebf4a171cb0a604b14a9cb7
HeoJunKu <lmkhkm@naver.com>
2020-08-05 16:21:26 +0900

    계기판 임시로 작동되게 만듬

---
+++ HeoJunKu/Dashboard/Assets/Scripts/NeedleMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedleMovement : MonoBehaviour
+{
+
+    RectTransform pannelImageRect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pannelImageRect = GameObject.Find("Velocity").GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(0, 0, -10), Space.Self);
+    }
+}
