4d1d35b0ed0c7591ec570264c361977050f32d32
HeoJunKu <lmkhkm@naver.com>
2020-08-05 12:20:30 +0900

    기존에 작업하던 파일 옮김

---
+++ HeoJunKu/MACHControlProject/MACHControlProject/MACHControlProject.ino
@@ -0,0 +1,23 @@
+﻿/*
+ Name:		MACHControlProject.ino
+ Created:	2020-07-29 오후 12:57:26
+ Author:	HeoJunKu
+*/
+
+// the setup function runs once when you press reset or power the board
+#include <RF24_config.h>
+#include <RF24.h>
+#include <printf.h>
+#include <nRF24L01.h>
+
+
+void setup() {
+
+
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
