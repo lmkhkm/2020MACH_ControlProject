3410f5ec05fc22f0aa1b1edfef1042752a1a8c40
girinssh <tngus5851@naver.com>
2020-08-12 16:10:52 +0900

    Create DataManager

---
+++ DataFormat/DataManager.cpp
@@ -0,0 +1,129 @@
+class DataManager {
+public:
+	// Acceleration Gyro Data
+	float gyro[3];
+	float accel[3];
+	float ypr[3];
+
+	// Pressure Data
+	float pressure[2];
+
+	// GPS Data
+	float latitude;
+	float longitude;
+	float atitude;
+	float speed;
+
+	// For Integrity
+	float integrityCode;
+
+	DataManager() {
+		for (int i = 0; i < 3; i++) {
+			gyro[i] = 0;
+			accel[i] = 0;
+			ypr[i] = 0;
+		}
+		pressure[0] = 0;
+		pressure[1] = 0;
+
+		latitude = 0;
+		longitude = 0;
+		atitude = 0;
+		speed = 0;
+	};
+
+	void setAccel(float* accel) {
+		for (int i = 0; i < 3; i++) {
+			this->accel[i] = accel[i];
+		}
+	}
+	void setGyro(float* gyro) {
+		for (int i = 0; i < 3; i++) {
+			this->gyro[i] = gyro[i];
+		}
+	}
+	void setPressure(float* pressure) {
+		this->pressure[0] = pressure[0];
+		this->pressure[1] = pressure[1];
+	}
+	void setGPS(float latitude, float longitude, float atitude, float speed) {
+		this->latitude = latitude;
+		this->longitude = longitude;
+		this->atitude = atitude;
+		this->speed = speed;
+	}
+
+	float* getDataWithFloatArray(int num) {
+		if (num == 1) {
+			float data[] = {
+				gyro[0],
+				gyro[1],
+				gyro[2],
+				accel[0],
+				accel[1],
+				accel[2],
+				pressure[0],
+				pressure[1]
+			};
+			return data;
+		}
+		else if (num == 2) {
+			float data[] = {
+				ypr[0],
+				ypr[1],
+				ypr[2],
+				latitude,
+				longitude,
+				atitude,
+				speed,
+				integrityCode
+			};
+			return data;
+		}
+	}
+	void setDataWithFloatArray(int num, float* dataset) {
+		if (num == 1) {
+			int i = 0;
+			for (int a = 0; a < 3; a++) {
+				gyro[a] = dataset[i];
+				i++;
+			}
+			for (int a = 0; a < 3; a++) {
+				accel[a] = dataset[i];
+				i++;
+			}
+			pressure[0] = dataset[6];
+			pressure[1] = dataset[7];
+		}
+		else if (num == 2) {
+			ypr[0] = dataset[0];
+			ypr[1] = dataset[1];
+			ypr[2] = dataset[2];
+			latitude = dataset[3];
+			longitude = dataset[4];
+			atitude = dataset[5];
+			speed = dataset[6];
+			integrityCode = dataset[7];
+		}
+	}
+private:
+	void makeIntegrityCode() {
+		integrityCode
+			= (int)gyro[0]
+			^ (int)gyro[1]
+			^ (int)gyro[2]
+			^ (int)accel[0]
+			^ (int)accel[1]
+			^ (int)accel[2]
+			^ (int)ypr[0]
+			^ (int)ypr[1]
+			^ (int)ypr[2]
+			^ (int)pressure[0]
+			^ (int)pressure[1]
+			^ (int)latitude
+			^ (int)longitude
+			^ (int)atitude
+			^ (int)speed;
+	}
+
+};
